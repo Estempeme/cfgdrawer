495659f454d823956b36343cd9cac334e09fd7ed
Ijjas Mihály <Ijjas Mihly>
2016-12-05 17:20:01 +0100

    Trying to use ps format.

--- Semesterprojekt_Uebung/src/Graph/Graph.java
+++ Semesterprojekt_Uebung/src/Graph/Graph.java
@@ -19,7 +19,8 @@ public class Graph {
 	
 	// A pointer to all of the nodes.
 	public Instruction firstinst;
-	private String path = "C:/Users/Misi HP/Documents/Iskola/Humboldt/programok/Semesterprojekt_Uebung/graph.png";
+	private String type = "ps";
+	private String path = "C:/Users/Misi HP/Documents/Iskola/Humboldt/programok/cfgdrawer/Semesterprojekt_Uebung/graph."+type;
 	
 	public Graph() {
 		this.firstinst = null;
@@ -116,7 +117,7 @@ public class Graph {
 		gv.addln(gv.end_graph());
 		System.out.println(gv.getDotSource());
 		File out = new File(path);
-		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), "png",
+		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type,
 				"dot"), out);
 		//Put picture to screen.
 		BufferedImage img = null;
@@ -165,8 +166,8 @@ public class Graph {
 	
 	public static void main(String[] args) {
 		Graph g = new Graph();
-		/*
-		g.addEdge(5, 1);
+		
+		/*g.addEdge(5, 1);
 		g.addEdge(1, 2);
 		g.addEdge(1, 3);
 		g.addEdge(2, 4);
@@ -174,14 +175,20 @@ public class Graph {
 		g.addEdge(3, 1);
 		g.addEdge(1, 2);
 		g.addEdge(2, 4);*/
-		/*for (int i = 0; i < 1000; i++) { //Extrem case.
+		for (int i = 0; i < 1000; i++) { //Extrem case.
 			g.addEdge(i, (i+1));
+		}
+		/*int n = 50;
+		for (int i = 0; i < n; i++) {
+			int a = (int) (Math.random()*n);
+			int b = (int) (Math.random()*n);
+			g.addEdge(a, b);
 		}*/
-		for (int i = 0; i < 10; i++) {
+		/*for (int i = 0; i < 10; i++) {
 			for (int j = i; j < 10; j++) {
 				g.addEdge(i, j);
 			}
-		}
+		}*/
 		g.createPicture();
 	}
 	
