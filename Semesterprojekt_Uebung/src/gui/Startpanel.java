540c22e8698333b1bf0a42df50042cb8cb6e7383
berweilj <janberw.eiler@gmail.com>
2016-12-17 15:09:02 +0100

    Radiobutton filetype added

--- Semesterprojekt_Uebung/src/gui/Startpanel.java
+++ Semesterprojekt_Uebung/src/gui/Startpanel.java
@@ -3,6 +3,7 @@
  */
 package gui;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
@@ -10,10 +11,13 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
@@ -24,6 +28,7 @@ import javax.swing.JPopupMenu;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -49,7 +54,7 @@ public class Startpanel extends JFrame {
 	
 	public Startpanel() {
 				
-		panel1 = new JPanel(new GridBagLayout());
+		panel1 = new JPanel(new FlowLayout());
 
 		
 		//getContentPane().add(panel2, 1);
@@ -65,6 +70,49 @@ public class Startpanel extends JFrame {
 		mnNewMenu.add(mntmBeedtruiea);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
+		//Create the radio buttons for the output type selection
+		
+	    JRadioButton pdf = new JRadioButton("pdf");
+	    pdf.setMnemonic(KeyEvent.VK_B);
+	    pdf.setActionCommand("pdf");
+	    pdf.setSelected(true);
+	    
+	    JRadioButton png = new JRadioButton("png");
+	    png.setMnemonic(KeyEvent.VK_B);
+	    png.setActionCommand("png");
+	    png.setSelected(false);
+	    
+	    JRadioButton ps = new JRadioButton("ps");
+	    ps.setMnemonic(KeyEvent.VK_B);
+	    ps.setActionCommand("ps");
+	    ps.setSelected(false);
+	    
+
+	    //Group the radio buttons.
+	    ButtonGroup group = new ButtonGroup();
+	    group.add(pdf);
+	    group.add(png);
+	    group.add(ps);
+	    
+	    ActionListener listenerSelectOutputType = new ActionListener(){
+	    	public void actionPerformed(ActionEvent e){
+	    		System.out.println("Fileformat set to: ");
+	    	}
+	    }; 
+	    
+	    //Register a listener for the radio buttons.
+	    pdf.addActionListener(listenerSelectOutputType);
+	    png.addActionListener(listenerSelectOutputType);
+	    ps.addActionListener(listenerSelectOutputType);
+	    
+	    JPanel FileTypePanel = new JPanel();
+	    FileTypePanel.add(pdf);
+	    FileTypePanel.add(png);
+	    FileTypePanel.add(ps);
+	    
+	    //getContentPane().add(FileTypePanel);
+	    
+	    panel1.add(FileTypePanel);
 		//lblNewLabel = new JLabel("New label");
 		//getContentPane().add(lblNewLabel);
 		
@@ -137,4 +185,6 @@ public class Startpanel extends JFrame {
 		}
 		return inputPath;
 	}
+	
+	
 }
