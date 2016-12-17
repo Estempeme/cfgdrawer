c93ebcb8f177435c84a3adfcec735cdd1fd54aac
berweilj <janberw.eiler@gmail.com>
2016-12-17 11:26:01 +0100

    try to import other projects

--- Semesterprojekt_Uebung/src/Graph/Instruction.java
+++ Semesterprojekt_Uebung/src/Graph/Instruction.java
@@ -1,16 +1,16 @@
 package Graph;
 
 public class Instruction {
-	
+
 	private final int id; // ID of the vertex
-	private Instruction next; // pointer to the next vertex in the list (has nothing to do with the graph)
+	private Instruction next; // pointer to the next vertex in the list (has
+								// nothing to do with the graph)
 	private Edge edges;
 	private String type;
 	private String error;
-	
-	
-	// By initializing a new Instruction we already received an edge 
-	//that leads from it to an other one.
+
+	// By initializing a new Instruction we already received an edge
+	// that leads from it to an other one.
 	public Instruction(int from, int to) {
 		this.id = from;
 		this.next = null;
@@ -18,14 +18,14 @@ public class Instruction {
 		this.type = "";
 		this.error = "";
 	}
-	
+
 	public void addEdge(int to) {
-		//This part should never occur, but who knows.
+		// This part should never occur, but who knows.
 		if (edges == null) { // There are no edges yet.
 			edges = new Edge(to);
 			return;
 		}
-		
+
 		Edge edge = searchEdge(to);
 		if (edge == null) { // Not found. Add new edge.
 			if (edges.getID() > to) { // New edge will be the first.
@@ -37,7 +37,7 @@ public class Instruction {
 			edge = edges;
 			while (edge.getNext() != null) {
 				if (edge.getNext().getID() > to) {
-					//Now insert a new Edge after the Edge "edge".
+					// Now insert a new Edge after the Edge "edge".
 					Edge temp = edge.getNext();
 					Edge newEdge = new Edge(to);
 					edge.setNext(newEdge);
@@ -49,25 +49,27 @@ public class Instruction {
 			// The new edge will be the last one.
 			Edge newEdge = new Edge(to);
 			edge.setNext(newEdge);
-			
+
 		} else {
 			edge.incNum();
 		}
 	}
-	
+
 	// Returns the searched Edge or null, if it doesn't exist.
 	public Edge searchEdge(int id) {
 		if (edges == null)
 			return null;
 		Edge edge = edges;
 		while (edge != null) {
-			if (edge.getID() == id) return edge; // Edge found.
-			if (edge.getID() > id) return null; // Id is too big: no such edge.
+			if (edge.getID() == id)
+				return edge; // Edge found.
+			if (edge.getID() > id)
+				return null; // Id is too big: no such edge.
 			edge = edge.getNext();
 		}
 		return null; // There is no edge to this node (yet).
 	}
-	
+
 	public void setNextInst(Instruction instr) {
 		this.next = instr;
 	}
@@ -75,19 +77,19 @@ public class Instruction {
 	public Edge getEdge() {
 		return this.edges;
 	}
-	
+
 	public Instruction getNextInst() {
 		return this.next;
 	}
-	
+
 	public int getID() {
 		return this.id;
 	}
-	
+
 	public String getType() {
 		return this.type;
 	}
-	
+
 	public String getError() {
 		return this.error;
 	}
--- Semesterprojekt_Uebung/src/Graph/pipeInputThread.java
+++ Semesterprojekt_Uebung/src/Graph/pipeInputThread.java
@@ -3,29 +3,27 @@ package Graph;
 import java.io.IOException;
 import java.io.PipedWriter;
 
-
 /*
  * This is a class that generates information for a fake input graph.
  */
 public class pipeInputThread extends Thread {
 	private PipedWriter pw;
-	
+
 	public pipeInputThread(PipedWriter pw) {
 		this.pw = pw;
-	} 
-	
-	
+	}
+
 	public void run() {
 		try {
 			System.out.println("Writing information.");
-			pw.write ("t1-21;2-21;");
+			pw.write("t1-21;2-21;");
 			pw.write("2-  2345;");
 			pw.write("2  -2345;");
-			//pw.write ("2-3");
-			pw.close ();
+			// pw.write ("2-3");
+			pw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
